c58e251cb46119e1722743ead1cd82ab69b4660f
Ruthie <ruthlessrebekah@gmail.com>
2025-04-06 19:34:35 -0500

    P4 S3 update 1

--- scripts/index.js
+++ scripts/index.js
@@ -30,12 +30,28 @@ console.log(initialCards)
 const editProfileBtn = document.querySelector('.profile__edit-btn')
 const editProfileModal = document.querySelector('#edit-profile-modal')
 const editProfileCloseBtn = editProfileModal.querySelector('.modal__close-btn')
+const editProfileForm = editProfileModal.querySelector('.modal__form')
+const editProfileNameInput = editProfileModal.querySelector(
+  '#profile-name-input',
+)
+const editProfileDescriptionInput = editProfileModal.querySelector(
+  '#profile-description-input',
+)
 
 const newPostBtn = document.querySelector('.profile__add-btn')
 const newPostModal = document.querySelector('#new-post-modal')
 const newPostCloseBtn = newPostModal.querySelector('.modal__close-btn')
+const newPostForm = newPostModal.querySelector('.modal__form')
+const newPostImageInput = newPostModal.querySelector('#card-image-input')
+const newPostCaptionInput = newPostModal.querySelector('#caption-input')
+
+const profileNameEl = document.querySelector('.profile__name')
+const profileDescriptionEl = document.querySelector('.profile__description')
 
 editProfileBtn.addEventListener('click', function () {
+  editProfileNameInput.value = profileNameEl.textContent
+  editProfileDescriptionInput.value = profileDescriptionEl.textContent
+
   editProfileModal.classList.add('modal_is-opened')
 })
 
@@ -50,3 +66,21 @@ newPostBtn.addEventListener('click', function () {
 newPostCloseBtn.addEventListener('click', function () {
   newPostModal.classList.remove('modal_is-opened')
 })
+
+function handleEditProfileSubmit(evt) {
+  evt.preventDefault()
+  profileNameEl.textContent = editProfileNameInput.value
+  profileDescriptionEl.textContent = editProfileDescriptionInput.value
+  editProfileModal.classList.remove('modal_is-opened')
+}
+
+editProfileForm.addEventListener('submit', handleEditProfileSubmit)
+
+function handleNewPostSubmit(evt) {
+  evt.preventDefault()
+  console.log(newPostImageInput)
+  console.log(newPostCaptionInput)
+  newPostModal.classList.remove('modal_is-opened')
+}
+
+newPostModal.addEventListener('submit', handleNewPostSubmit)
