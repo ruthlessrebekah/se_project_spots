c465e4e388d34ee13380ab689814a6bc4857eaa7
Ruthie <ruthlessrebekah@gmail.com>
2025-04-20 19:52:59 -0500

    feat: Add preview modal functionality and responsive styling

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,9 @@
 const initialCards = [
   {
+    name: 'Golden Gate Bridge',
+    link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/7-photo-by-griffin-wooldridge-from-pexels.jpg',
+  },
+  {
     name: 'Val Thorens',
     link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg',
   },
@@ -43,6 +47,46 @@ const newPostCaptionInput = newPostForm.elements['caption']
 const profileNameEl = document.querySelector('.profile__name')
 const profileDescriptionEl = document.querySelector('.profile__description')
 
+const previewModal = document.querySelector('#preview-modal')
+const previewModalCloseBtn = previewModal.querySelector('.modal__close-btn')
+const previewImageEl = previewModal.querySelector('.modal__image')
+const previewCaptionEl = previewModal.querySelector('.modal__caption')
+
+const cardTemplate = document
+  .querySelector('#card-template')
+  .content.querySelector('.card')
+const cardsList = document.querySelector('.cards__list')
+
+function getCardElement(data) {
+  const cardElement = cardTemplate.cloneNode(true)
+  const cardTitleEl = cardElement.querySelector('.card__title')
+  const cardImageEl = cardElement.querySelector('.card__image')
+
+  cardImageEl.src = data.link
+  cardImageEl.alt = data.name
+  cardTitleEl.textContent = data.name
+
+  const cardLikeBtnEl = cardElement.querySelector('.card__like-btn')
+  cardLikeBtnEl.addEventListener('click', () => {
+    cardLikeBtnEl.classList.toggle('card__like-btn_active')
+  })
+
+  const cardDeleteBtnEl = cardElement.querySelector('.card__delete-btn')
+  cardDeleteBtnEl.addEventListener('click', () => {
+    cardElement.remove()
+    cardElement = null
+  })
+
+  cardImageEl.addEventListener('click', () => {
+    previewImageEl.src = data.link
+    previewImageEl.alt = data.name
+    previewCaptionEl.textContent = data.name
+    openModal(previewModal)
+  })
+
+  return cardElement
+}
+
 function openModal(modal) {
   modal.classList.add('modal_is-opened')
 }
@@ -59,6 +103,10 @@ editProfileBtn.addEventListener('click', function () {
 
 editProfileCloseBtn.addEventListener('click', function () {
   closeModal(editProfileModal)
+})
+
+previewModalCloseBtn.addEventListener('click', function () {
+  closeModal(previewModal)
 })
 
 newPostBtn.addEventListener('click', function () {
@@ -84,8 +132,15 @@ function handleEditProfileSubmit(evt) {
 
 function handleNewPostSubmit(evt) {
   evt.preventDefault()
-  console.log(newPostImageInput)
-  console.log(newPostCaptionInput)
+
+  const inputValues = {
+    name: newPostCaptionInput.value,
+    link: newPostImageInput.value,
+  }
+
+  const cardElement = getCardElement(inputValues)
+  cardsList.prepend(cardElement)
+
   newPostModal.classList.remove('modal_is-opened')
 }
 
@@ -93,6 +148,6 @@ newPostForm.addEventListener('submit', handleNewPostSubmit)
 editProfileForm.addEventListener('submit', handleEditProfileSubmit)
 
 initialCards.forEach(function (item) {
-  console.log(item.name)
-  console.log(item.link)
+  const cardElement = getCardElement(item)
+  cardsList.append(cardElement)
 })
